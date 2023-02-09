874e40c96b622db737a3583ae14277c1189f34bc
Acumen <82719716+Acumen-Protocol@users.noreply.github.com>
2023-02-08 19:41:00 -0500

    Update CollateralToken.sol
    Updated Reserve Contract

--- CollateralToken.sol
+++ CollateralToken.sol
@@ -7,44 +7,41 @@ import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
+interface IStaking {
+    function handleCollateralTokenTransfer(
+        uint256 _pid,
+        address from,
+        address to,
+        uint256 amount
+    ) external;
+}
+
 contract Reserve is ERC20, ERC20Burnable, Ownable {
     using SafeERC20 for IERC20;
 
-    address private immutable tokenContract;
-    address private immutable stakerContract;
+    address private immutable _tokenContract;
+    address private immutable _stakerContract;
     uint8 private immutable _decimals;
+    uint256 private immutable _pid;
 
     constructor(
-        address _stakerContract,
-        address _tokenContract,
-        uint8 _tokenDecimals
+        address stakerContract,
+        address tokenContract,
+        uint8 _tokenDecimals,
+        uint256 pid
     ) ERC20('Staking Collateral Token', 'SCT') {
         _decimals = _tokenDecimals;
 
-        _transferOwnership(_stakerContract);
+        _transferOwnership(stakerContract);
 
-        tokenContract = _tokenContract;
-        stakerContract = _stakerContract;
-    }
+        _tokenContract = tokenContract;
+        _stakerContract = stakerContract;
 
-    function decimals() public view virtual override returns (uint8) {
-        return _decimals;
-    }
-
-    function burn(uint256 amount) public virtual override onlyOwner {
-        _burn(_msgSender(), amount);
-    }
-
-    function burnFrom(
-        address account,
-        uint256 amount
-    ) public virtual override onlyOwner {
-        _spendAllowance(account, _msgSender(), amount);
-        _burn(account, amount);
+        _pid = pid;
     }
 
     function resetAllowance() external onlyOwner {
-        IERC20(tokenContract).approve(stakerContract, type(uint256).max);
+        IERC20(_tokenContract).approve(_stakerContract, type(uint256).max);
     }
 
     function mintCollateralToken(
@@ -64,18 +61,99 @@ contract Reserve is ERC20, ERC20Burnable, Ownable {
 
         return true;
     }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        if (msg.sender != _stakerContract) {
+            require(to != address(this), 'Should not transfer to reserve!');
+        }
+
+        address spender = _msgSender();
+
+        _spendAllowance(from, spender, amount);
+
+        _transfer(from, to, amount);
+
+        if (
+            from != address(this) &&
+            from != _stakerContract &&
+            to != _stakerContract
+        ) {
+            IStaking(_stakerContract).handleCollateralTokenTransfer(
+                _pid,
+                from,
+                to,
+                amount
+            );
+        }
+
+        return true;
+    }
+
+    function transfer(
+        address to,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        if (msg.sender != _stakerContract) {
+            require(to != address(this), 'Should not transfer to reserve!');
+        }
+
+        address owner = _msgSender();
+
+        _transfer(owner, to, amount);
+
+        if (
+            owner != address(this) &&
+            owner != _stakerContract &&
+            to != _stakerContract
+        ) {
+            IStaking(_stakerContract).handleCollateralTokenTransfer(
+                _pid,
+                owner,
+                to,
+                amount
+            );
+        }
+
+        return true;
+    }
+
+    /**
+     * @dev Override reason for burn functions: 
+     * Only the staker contract should have the authority to burn
+     */
+    function burn(uint256 amount) public virtual override onlyOwner {
+        _burn(_msgSender(), amount);
+    }
+    
+    function burnFrom(
+        address account,
+        uint256 amount
+    ) public virtual override onlyOwner {
+        _spendAllowance(account, _msgSender(), amount);
+        _burn(account, amount);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
 }
 
 contract ReserveDeployer {
     function createReserve(
         address _stakerContract,
         address _tokenContract,
-        uint8 _tokenDecimals
+        uint8 _tokenDecimals,
+        uint256 _pid
     ) external returns (address) {
         Reserve _deployed = new Reserve(
             _stakerContract,
             _tokenContract,
-            _tokenDecimals
+            _tokenDecimals,
+            _pid
         );
 
         return address(_deployed);
